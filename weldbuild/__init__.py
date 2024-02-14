b2b422e3ad370978e4f3810b53b12722efbe2416
Dmitriy Lukovenko <mludima23@gmail.com>
2024-02-14 20:47:27 +0800

    1.0.3

--- weldbuild/__init__.py
+++ weldbuild/__init__.py
@@ -6,6 +6,7 @@ import datetime
 
 Version = tuple[int, int, int]
 
+
 class ArchType:
     ARM64 = 0
     ARM = 1
@@ -22,7 +23,7 @@ class ArchType:
                 return "x86_64"
             case ArchType.X86:
                 return "x86"
-        
+
 
 class PlatformType:
     Windows = 0
@@ -37,7 +38,7 @@ class PlatformType:
                 return "linux"
             case PlatformType.Android:
                 return "android"
-            
+
 
 class BuildPlatform:
     def __init__(self, platform: PlatformType):
@@ -45,63 +46,68 @@ class BuildPlatform:
         self.arch = None
         self.site_packages = []
 
-
     def configure(self, arch: ArchType):
         self.arch = arch
 
         clean = True
 
-        if os.path.exists(os.path.join(os.getcwd(), "build", "intermediate", ".configure")):
-            with open(os.path.join(os.getcwd(), "build", "intermediate", ".configure"), "rt") as fp:
+        if os.path.exists(
+            os.path.join(os.getcwd(), "build", "intermediate", ".configure")
+        ):
+            with open(
+                os.path.join(os.getcwd(), "build", "intermediate", ".configure"), "rt"
+            ) as fp:
                 data = fp.read()
-                _, value = data.split('=')
-                
+                _, value = data.split("=")
+
                 if value == ArchType.name(arch):
                     clean = False
 
         if clean:
             shutil.rmtree(os.path.join(os.getcwd(), "build"), ignore_errors=True)
 
-            os.makedirs(os.path.join(os.getcwd(), "build", "intermediate"), exist_ok=True)
+            os.makedirs(
+                os.path.join(os.getcwd(), "build", "intermediate"), exist_ok=True
+            )
 
-            with open(os.path.join(os.getcwd(), "build", "intermediate", ".configure"), "wt") as fp:
+            with open(
+                os.path.join(os.getcwd(), "build", "intermediate", ".configure"), "wt"
+            ) as fp:
                 fp.write(f"ARCH={ArchType.name(arch)}")
 
-    
     def add_site_packages(self, packages: list[str]):
         self.site_packages.extend(packages)
 
-    
     def build(self):
         pass
 
 
 class Library:
-    def __init__(
-        self,
-        name: str,
-        platform: PlatformType,
-        version: str
-    ):
+    def __init__(self, name: str, platform: PlatformType, version: str):
         self.name = name
         self.platform = platform
         self.version = version
 
         self.out_path = os.path.join(os.getcwd(), "build", "intermediate", "deps")
         os.makedirs(self.out_path, exist_ok=True)
-        
 
     def path(self) -> str:
         return f"{self.name}-{self.version}"
-    
-        
+
     def unpack(self):
         if os.path.exists(os.path.join(self.out_path, self.path())):
             return
 
         print(f" Unpack {self.path()}-{PlatformType.name(self.platform)}.zip")
 
-        with zipfile.ZipFile(os.path.join(os.path.dirname(__file__), "deps", f"{self.path()}-{PlatformType.name(self.platform)}.zip"), "r") as zipfp:
+        with zipfile.ZipFile(
+            os.path.join(
+                os.path.dirname(__file__),
+                "deps",
+                f"{self.path()}-{PlatformType.name(self.platform)}.zip",
+            ),
+            "r",
+        ) as zipfp:
             zipfp.extractall(os.path.join(self.out_path, self.path()))
 
 
@@ -117,17 +123,20 @@ def archive_package(src_path: str, version: Version, output_path: str) -> str:
     # Create .VERSION file - VERSION_STRING.VERSION_TIMESTAMP
     with open(os.path.join(src_path, ".VERSION"), "wt") as fp:
         fp.write(f"{version_string}.{version_timestamp}")
-    
+
     # Create ZIP archive
     with zipfile.ZipFile(os.path.join(output_path), "w", zipfile.ZIP_STORED) as zipfp:
         for path, _, files in os.walk(src_path):
             for file in files:
-                zipfp.write(os.path.join(path, file), os.path.relpath(os.path.join(path, file), src_path))
+                zipfp.write(
+                    os.path.join(path, file),
+                    os.path.relpath(os.path.join(path, file), src_path),
+                )
 
     return f"{version_string}.{version_timestamp}"
 
 
-def copy_compile(src_path: str, out_path: str, dir_name: str = '') -> int:
+def copy_compile(src_path: str, out_path: str, dir_name: str = "") -> int:
     def get_sources() -> list[str]:
         out = []
 
@@ -136,19 +145,20 @@ def copy_compile(src_path: str, out_path: str, dir_name: str = '') -> int:
                 if not file.endswith(".pyc"):
                     out.append(os.path.join(path, file))
         return out
-    
 
     def get_database() -> dict:
         out = {}
 
-        with open(os.path.join(os.getcwd(), "build", "intermediate", ".sources"), "rt") as fp:
+        with open(
+            os.path.join(os.getcwd(), "build", "intermediate", ".sources"), "rt"
+        ) as fp:
             while True:
-                line = fp.readline().strip('\n')
-            
-                if line == None or line == '':
+                line = fp.readline().strip("\n")
+
+                if line == None or line == "":
                     break
 
-                data = line.split(',')
+                data = line.split(",")
 
                 file_path = data[0]
                 file_size = int(data[1])
@@ -156,7 +166,6 @@ def copy_compile(src_path: str, out_path: str, dir_name: str = '') -> int:
 
                 out[file_path] = (file_size, file_mtime)
         return out
-
 
     def filter_sources(sources: list[str], database: dict) -> list[str]:
         out = []
@@ -174,10 +183,11 @@ def copy_compile(src_path: str, out_path: str, dir_name: str = '') -> int:
                 database[source] = (scan_size, scan_mtime)
                 out.append(source)
         return out
-    
-    
+
     def save_database_to_file(database: dict):
-        with open(os.path.join(os.getcwd(), "build", "intermediate", ".sources"), 'wt') as fp:
+        with open(
+            os.path.join(os.getcwd(), "build", "intermediate", ".sources"), "wt"
+        ) as fp:
             for key, value in database.items():
                 fp.write(f"{key},{value[0]},{value[1]}\n")
 
@@ -200,20 +210,32 @@ def copy_compile(src_path: str, out_path: str, dir_name: str = '') -> int:
     for source in sources:
         if source.endswith(".py"):
             try:
-                print(f" Build {source}") 
+                print(f" Build {source}")
                 py_compile.compile(
-                    source, 
-                    os.path.join(out_path, dir_name, os.path.relpath(source.removesuffix(".py") + ".pyc", src_path)), 
-                    doraise=True, 
-                    quiet=1
+                    source,
+                    os.path.join(
+                        out_path,
+                        dir_name,
+                        os.path.relpath(source.removesuffix(".py") + ".pyc", src_path),
+                    ),
+                    doraise=True,
+                    quiet=1,
                 )
                 size += 1
             except py_compile.PyCompileError as e:
                 raise RuntimeError(f" Build failed!\n{e}")
         else:
             print(f" Copy {source}")
-            os.makedirs(os.path.dirname(os.path.join(out_path, dir_name, os.path.relpath(source, src_path))), exist_ok=True)
-            shutil.copyfile(source, os.path.join(out_path, dir_name, os.path.relpath(source, src_path)))
+            os.makedirs(
+                os.path.dirname(
+                    os.path.join(out_path, dir_name, os.path.relpath(source, src_path))
+                ),
+                exist_ok=True,
+            )
+            shutil.copyfile(
+                source,
+                os.path.join(out_path, dir_name, os.path.relpath(source, src_path)),
+            )
             size += 1
 
     # Rewrite database if files has been changed
--- weldbuild/platforms/win32/cpp/src/main.cpp
+++ weldbuild/platforms/win32/cpp/src/main.cpp
@@ -1,18 +1,23 @@
-#include "precompiled.h"
 #include "app.h"
+#include "precompiled.h"
 #include <Python.h>
 
-int main(int argc, char** argv) {
-    try {
+auto main(int32_t argc, char** argv) -> int32_t
+{
+    try
+    {
         App app;
-        if(!app.check_install()) {
+        if (!app.check_install())
+        {
             app.install();
         }
-    } catch(std::runtime_error e) {
+    }
+    catch (std::runtime_error e)
+    {
         ::MessageBox(nullptr, e.what(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
-    
+
     std::filesystem::path const internal_path = std::filesystem::current_path();
     std::filesystem::path const app_path = internal_path / ".app";
 
@@ -23,46 +28,53 @@ int main(int argc, char** argv) {
     PyConfig_InitIsolatedConfig(&config);
 
     status = PyConfig_SetBytesString(&config, &config.program_name, "App");
-    if (PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     config.module_search_paths_set = 1;
     config.site_import = 0;
-    
+
     status = PyWideStringList_Append(&config.module_search_paths, (app_path / "site-packages").wstring().c_str());
-    if(PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     status = PyWideStringList_Append(&config.module_search_paths, (app_path / "modules").wstring().c_str());
-    if(PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     status = PyWideStringList_Append(&config.module_search_paths, (app_path / "stdlib.zip").wstring().c_str());
-    if(PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     status = PyConfig_SetBytesString(&config, &config.executable, (app_path / "python.exe").string().c_str());
-    if(PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     status = PyConfig_SetBytesString(&config, &config.run_filename, (app_path / "__boot__.pyc").string().c_str());
-    if(PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     status = Py_InitializeFromConfig(&config);
-    if (PyStatus_Exception(status)) {
+    if (PyStatus_Exception(status))
+    {
         ::MessageBox(nullptr, std::format("{}", status.err_msg).c_str(), "App", MB_ICONERROR);
         return EXIT_FAILURE;
     }
